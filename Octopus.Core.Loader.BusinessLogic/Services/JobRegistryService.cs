e240b23cc3c25580265e6b158375e4da88411d5a
Vladititov <capt.Hoffmann@gmail.com>
2021-12-16 15:33:17 +0300

    start implementing IRabbitMqListener in Loader project

--- Octopus.Core.Loader.BusinessLogic/Services/JobRegistryService.cs
+++ Octopus.Core.Loader.BusinessLogic/Services/JobRegistryService.cs
@@ -1,13 +1,14 @@
 ﻿using FluentScheduler;
 using Octopus.Core.Loader.BusinessLogic.Jobs;
+using Octopus.Core.RabbitMq.Services.Interfaces;
 
 namespace Octopus.Core.Loader.BusinessLogic.Services
 {
     public class JobRegistryService : Registry
     {
-        public JobRegistryService()
+        public JobRegistryService(IRabbitMqListener listener)
         {
-            Schedule(() => new QueueConsumer())
+            Schedule(() => new QueueConsumer(listener))
                 .NonReentrant()
                 .ToRunNow()
                 .AndEvery(1)
