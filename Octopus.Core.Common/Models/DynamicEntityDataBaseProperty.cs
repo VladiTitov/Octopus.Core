1c6fc23b659ceddd55f0cbe6cd69b7ff066be90e
Vasili\VasiliK <kottok1998@gmail.com>
2021-12-20 16:25:35 +0300

    + added DynamicTypeFactory

---
+++ Octopus.Core.Common/Models/DynamicEntityDataBaseProperty.cs
@@ -0,0 +1,15 @@
+﻿namespace Octopus.Core.Common.Models
+{
+    public class DynamicEntityDataBaseProperty
+    {
+        public string DataBaseTypeName { get; set; }
+
+        public int Length { get; set; }
+
+        public bool IsNotNull { get; set; }
+
+        public bool IsKey { get; set; }
+
+        public string Comment { get; set; }
+    }
+}
---
+++ Octopus.Core.Common/Models/DynamicProperty.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text.Json.Serialization;
+
+namespace Octopus.Core.Common.Models
+{
+    public class DynamicProperty
+    {
+        public string PropertyName { get; set; }
+        public string SystemTypeName { get; set; }
+        public int ValueIndex { get; set; }
+
+        [JsonIgnore]
+        public Type SystemType => Type.GetType(SystemTypeName);
+
+        public DynamicEntityDataBaseProperty DynamicEntityDataBaseProperty { get; set; }
+    }
+}
