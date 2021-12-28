7dcac8fc2381c94400344306adb0c4ac35277bd2
Vladislav Titov <capt.Hoffmann@gmail.com>
2021-12-28 22:08:19 +0300

    added the possibility of external configuration of the exchange type and the routingKey parameter

--- Octopus.Core.Common/ConfigsModels/RabbitMqConfiguration.cs
+++ Octopus.Core.Common/ConfigsModels/RabbitMqConfiguration.cs
@@ -4,8 +4,10 @@
     {
         public string Hostname { get; set; }
         public int Port { get; set; }
+        public string ExchangeType { get; set; }
         public string ExchangeName { get; set; }
         public string QueueName { get; set; }
+        public string RoutingKey { get; set;}
         public string UserName { get; set; }
         public string Password { get; set; }
     }
