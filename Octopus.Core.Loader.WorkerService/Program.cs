4d5af4fb046ce038996e8247d5e8d4010081275b
Vladititov <capt.Hoffmann@gmail.com>
2022-01-18 17:53:00 +0300

    added sample file with connection configuration and logic for working with it

--- Octopus.Core.Loader.WorkerService/Program.cs
+++ Octopus.Core.Loader.WorkerService/Program.cs
@@ -1,8 +1,10 @@
+using System.Collections.Generic;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Octopus.Core.Common.ConfigsModels.ConnectionStrings;
 using Octopus.Core.Common.ConfigsModels.Rabbit;
+using Octopus.Core.Common.ConfigsModels.Rabbit.Base;
 using Octopus.Core.Common.DynamicObject.Services;
 using Octopus.Core.Common.DynamicObject.Services.Interfaces;
 using Octopus.Core.Common.Helpers.JsonDeserializer;
@@ -14,6 +16,7 @@ using Octopus.Core.Loader.DataAccess.Interfaces;
 using Octopus.Core.Loader.DataAccess.Repositories;
 using Octopus.Core.Loader.DataAccess.Services;
 using Octopus.Core.RabbitMq.Context;
+using Octopus.Core.RabbitMq.Services;
 using Octopus.Core.RabbitMq.Services.Implementations;
 using Octopus.Core.RabbitMq.Services.Interfaces;
 
@@ -33,21 +36,19 @@ namespace Octopus.Core.Loader.WorkerService
                     confBuilder.AddJsonFile("appsettings.json");
                     confBuilder.AddJsonFile("Configs/rabbitMq-config.json");
                     confBuilder.AddJsonFile("Configs/connectionString-config.json");
+                    confBuilder.AddJsonFile("Configs/rabbitMqConnectionString-config.json");
                 })
                 .ConfigureServices((hostContext, services) =>
                 {
                     services.Configure<RabbitMqConfiguration>(hostContext.Configuration.GetSection("RabbitParams"));
                     services.Configure<ConnectionStringConfig>(hostContext.Configuration.GetSection("ConnectionString"));
-                 
-                    services.Configure<RabbitMqConfiguration>(hostContext.Configuration.GetSection("RabbitMqSubscriber"));
-                    services.Configure<RabbitMqConfiguration>(hostContext.Configuration.GetSection("RabbitMqPublisher"));
+                    services.Configure<ConnectionConfiguration>(hostContext.Configuration.GetSection("RabbitMqConnectionString"));
 
-                    services.AddSingleton<IRabbitMqContext, RabbitMqContext>();
-                    services.AddSingleton<IRabbitMqPublisher, RabbitMqPublisher>();
-                    services.AddSingleton<IEventProcessor, MessageHandler>();
+                    services.AddSingleton(hostContext.Configuration.GetSection("Subscribers").Get<IEnumerable<SubscriberConfiguration>>());
+                    services.AddSingleton(hostContext.Configuration.GetSection("Publishers").Get<IEnumerable<PublisherConfiguration>>());
+
                     services.AddSingleton<IDataReaderService, DataReaderService>();
                     services.AddSingleton<IEntityDescription, EntityDescription>();
-
                     services.AddSingleton<IDynamicObjectCreateService, DynamicObjectCreateService>();
                     services.AddSingleton<IDynamicTypeFactory, DynamicTypeFactory>();
                     services.AddSingleton<IDynamicEntityService, DynamicEntityService>();
@@ -62,6 +63,17 @@ namespace Octopus.Core.Loader.WorkerService
                     services.AddSingleton<IQueryHandlerService, QueryHandlerService>();
 
                     services.AddSingleton<IDatabaseContext, DapperDbContext>();
+
+                    #region RabbitMQ
+
+                    services.AddSingleton<IRabbitMqContext, RabbitMqContext>();
+
+                    services.AddSingleton<IRabbitMqInitializer, RabbitMqInitializer>();
+                    services.AddSingleton<IRabbitMqSubscriber, RabbitMqSubscriber>();
+                    services.AddSingleton<IRabbitMqPublisher, RabbitMqPublisher>();
+                    services.AddSingleton<IEventProcessor, MessageHandler>();
+
+                    #endregion
 
                     services.AddHostedService<LoaderService>();
                 });
