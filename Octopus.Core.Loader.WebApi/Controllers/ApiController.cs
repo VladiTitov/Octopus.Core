40117b122dc2134526cc9fd34013aa19da7be747
Vladititov <capt.Hoffmann@gmail.com>
2022-01-20 16:12:27 +0300

    initial commit

---
+++ Octopus.Core.Loader.WebApi/Controllers/ApiController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Octopus.Core.Loader.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ApiController : ControllerBase
+    {
+
+    }
+}
