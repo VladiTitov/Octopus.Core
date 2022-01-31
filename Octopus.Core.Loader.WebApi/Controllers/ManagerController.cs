a90713850ba98f076fbec3ad9a0109bed1e25930
Vladititov <capt.Hoffmann@gmail.com>
2022-01-31 15:12:05 +0300

    fixed bad controller name

--- Octopus.Core.Loader.WebApi/Controllers/ManagerController.cs
+++ Octopus.Core.Loader.WebApi/Controllers/ManagerController.cs
@@ -8,7 +8,7 @@ namespace Octopus.Core.Loader.WebApi.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ManageController : ControllerBase
+    public class ManagerController : ControllerBase
     {
 
     }
