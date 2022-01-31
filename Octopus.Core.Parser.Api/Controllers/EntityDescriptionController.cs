33e23747f123761ad33b4e6c4d86034a9ab97b3f
Vasilisk <kottok1998@gmail.com>
2022-01-31 15:18:04 +0300

    + added controllers

---
+++ Octopus.Core.Parser.Api/Controllers/EntityDescriptionController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Octopus.Core.Parser.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EntityDescriptionController : ControllerBase
+    {
+    }
+}
