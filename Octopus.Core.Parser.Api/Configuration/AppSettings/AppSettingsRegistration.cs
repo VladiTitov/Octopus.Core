ecb0e64c921b44f2792465a88b14c42a70e46c8b
Vasilisk <kottok1998@gmail.com>
2022-02-04 09:45:27 +0300

    + added configuration files to parser api

--- Octopus.Core.Parser.Api/Configuration/AppSettings/AppSettingsRegistration.cs
+++ Octopus.Core.Parser.Api/Configuration/AppSettings/AppSettingsRegistration.cs
@@ -1,7 +1,12 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Octopus.Core.Common.ApiConfigs;
+using Octopus.Core.Common.ConfigsModels.Rabbit;
+using Octopus.Core.Common.ConfigsModels.Rabbit.Base;
 using Octopus.Core.Common.Constants;
+using Octopus.Core.Parser.WorkerService.Configs.Implementations;
+using Octopus.Core.Parser.WorkerService.Configuration.Implementations;
+using System.Collections.Generic;
 
 namespace Octopus.Core.Parser.Api.Configuration.AppSettings
 {
@@ -10,6 +15,15 @@ namespace Octopus.Core.Parser.Api.Configuration.AppSettings
         public static IServiceCollection RegisterAppSettingsSections(this IServiceCollection services,
             IConfiguration configuration)
             => services
-                .Configure<ApplicationConfiguration>(configuration.GetSection(ConfigurationSectionNames.Application));
+                .Configure<ApplicationConfiguration>(configuration.GetSection(ConfigurationSectionNames.Application))
+                .Configure<ProcessorConfiguration>(configuration.GetSection(nameof(ProcessorConfiguration)))
+                .Configure<ConnectionConfiguration>(configuration.GetSection(ConfigurationSectionNames.RabbitConnection))
+                .Configure<PublisherConfiguration>(configuration.GetSection(ConfigurationSectionNames.Publisher))
+                .Configure<CsvParserConfiguration>(configuration.GetSection(nameof(CsvParserConfiguration)))
+                .Configure<JsonParserConfiguration>(configuration.GetSection(nameof(JsonParserConfiguration)))
+                .Configure<XmlParserConfiguration>(configuration.GetSection(nameof(XmlParserConfiguration)))
+                .AddSingleton(configuration.GetSection(ConfigurationSectionNames.Subscribers).Get<IEnumerable<SubscriberConfiguration>>());
+
+
     }
 }
--- Octopus.Core.Parser.Api/Program.cs
+++ Octopus.Core.Parser.Api/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Octopus.Core.Parser.Api.Extensions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -18,6 +19,10 @@ namespace Octopus.Core.Parser.Api
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((hostContext, confBuilder) =>
+                {
+                    confBuilder.AddJsonFiles(hostContext);
+                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
